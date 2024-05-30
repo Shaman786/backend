c6f45776185e2f1a4ea82efc81476e51773c7e6e
Fayaj Hossain <fayazhossain88@gmail.com>
2024-05-30 14:17:15 +0530

    add register controller and bug fixes

--- chai-backend/src/utils/asyncHandler.js
+++ chai-backend/src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((e) => next(e));
-    console.log(e);
+    // console.log(e);
   };
 };
 
