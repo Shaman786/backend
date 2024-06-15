dcbf5256f8a29ccb09fb1a0e265ef9da4ac014b6
Fayaj Hossain <fayazhossain88@gmail.com>
2024-06-15 16:39:24 +0530

    chore: Update user.model.js to fix typo in lowercase property

--- chai-backend/src/controllers/user.controller.js
+++ chai-backend/src/controllers/user.controller.js
@@ -109,6 +109,7 @@ const loginUser = asyncHandler(async (req, res) => {
   if (!user) {
     throw new ApiError(404, "User does not exist");
   }
+
   const isPasswordValid = await user.isPasswordCorrect(password);
 
   if (!isPasswordValid) {
